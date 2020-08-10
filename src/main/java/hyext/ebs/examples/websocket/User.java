db3ed71ef9231ae17bb6c19656c9a31de5cf697b
a961370183 <48462172+a961370183@users.noreply.github.com>
2020-08-10 23:44:33 +0800

    "second commit"

---
+++ src/main/java/hyext/ebs/examples/websocket/User.java
@@ -0,0 +1,103 @@
+package hyext.ebs.examples.websocket;
+
+public class User {
+    private int senderLevel;
+    private String unionId;
+    private String badgeName;
+    private String senderAvatarUrl;
+    private int showMode;
+    private int fansLevel;
+    private String sendNick;
+    private int nobleLevel;
+    private String content;
+    private int roomId;
+    public boolean senderGender;
+
+    public int getSenderLevel() {
+        return senderLevel;
+    }
+
+    public void setSenderLevel(int senderLevel) {
+        this.senderLevel = senderLevel;
+    }
+
+    public String getUnionId() {
+        return unionId;
+    }
+
+    public void setUnionId(String unionId) {
+        this.unionId = unionId;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
+
+    public void setBadgeName(String badgeName) {
+        this.badgeName = badgeName;
+    }
+
+    public String getSenderAvatarUrl() {
+        return senderAvatarUrl;
+    }
+
+    public void setSenderAvatarUrl(String senderAvatarUrl) {
+        this.senderAvatarUrl = senderAvatarUrl;
+    }
+
+    public int getShowMode() {
+        return showMode;
+    }
+
+    public void setShowMode(int showMode) {
+        this.showMode = showMode;
+    }
+
+    public int getFansLevel() {
+        return fansLevel;
+    }
+
+    public void setFansLevel(int fansLevel) {
+        this.fansLevel = fansLevel;
+    }
+
+    public String getSendNick() {
+        return sendNick;
+    }
+
+    public void setSendNick(String sendNick) {
+        this.sendNick = sendNick;
+    }
+
+    public int getNobleLevel() {
+        return nobleLevel;
+    }
+
+    public void setNobleLevel(int nobleLevel) {
+        this.nobleLevel = nobleLevel;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public boolean isSenderGender() {
+        return senderGender;
+    }
+
+    public void setSenderGender(boolean senderGender) {
+        this.senderGender = senderGender;
+    }
+}
